722d556577015b64ed2cc5d4e732fea23398c577
Saloni khandelwal <64362919+saloni45@users.noreply.github.com>
2026-06-19 13:36:36 +0530

    Increase WebDriverWait duration to 30 seconds

--- src/main/java/pages/AgodaFlightPage.java
+++ src/main/java/pages/AgodaFlightPage.java
@@ -12,7 +12,7 @@ public class AgodaFlightPage {
 
     public AgodaFlightPage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     public void openFlightsTab() {
